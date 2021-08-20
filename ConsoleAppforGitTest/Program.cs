fa2cbc61a56fbd152ef7e0163ecf35fec39315aa
Suresh <Suresh@DESKTOP-LPDV5QH>
2021-08-20 17:58:48 +0530

    Add project files.

---
+++ ConsoleAppforGitTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppforGitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b,c;
+            a = Int32.Parse(Console.ReadLine());
+            b = Int32.Parse(Console.ReadLine());
+            c = (a + b);
+            Console.WriteLine($"{c}");
+        }
+    }
+}
